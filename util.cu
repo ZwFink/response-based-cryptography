0cb15972abdc73d7c93177e2f44d0365550e7b34
Bob Costas <finkzane@gmail.com>
2019-09-23 21:51:42 -0700

    added macros, expand_key device function

--- util.cu
+++ util.cu
@@ -9,9 +9,106 @@
 #define NUM_BLOCKS 30
 #endif
 
+#ifdef _MSC_VER
+#define SWAP(x) (_lrotl(x, 8) & 0x00ff00ff | _lrotr(x, 8) & 0xff00ff00)
+#define GETWORD(p) SWAP(*((uint *)(p)))
+#define PUTWORD(ct, st) (*((uint *)(ct)) = SWAP((st)))
+#else
+#define GETWORD(pt) (((uint)(pt)[0] << 24) ^ ((uint)(pt)[1] << 16) ^ ((uint)(pt)[2] <<  8) ^ ((uint)(pt)[3]))
+#define PUTWORD(ct, st) ((ct)[0] = (uchar)((st) >> 24), (ct)[1] = (uchar)((st) >> 16), (ct)[2] = (uchar)((st) >>  8), (ct)[3] = (uchar)(st), (st))
+#endif
+
 #define NUM_THREADS 1024
 
 #define MAX_RK_SIZE 182
+__device__ void expand_key( const uchar *cipherKey,
+                            uint *e_sched,
+                            uint Nr,
+                            uint keyBits
+                          )
+{
+    uint *rek = e_sched;
+    uint i = 0;
+    uint temp;
+    rek[0] = GETWORD(cipherKey     );
+    rek[1] = GETWORD(cipherKey +  4);
+    rek[2] = GETWORD(cipherKey +  8);
+    rek[3] = GETWORD(cipherKey + 12);
+    if (keyBits == 128) {
+        for (;;) {
+            temp  = rek[3];
+            rek[4] = rek[0] ^
+                (cTe4[(temp >> 16) & 0xff] & 0xff000000) ^
+                (cTe4[(temp >>  8) & 0xff] & 0x00ff0000) ^
+                (cTe4[(temp      ) & 0xff] & 0x0000ff00) ^
+                (cTe4[(temp >> 24)       ] & 0x000000ff) ^
+                crcon[i];
+            rek[5] = rek[1] ^ rek[4];
+            rek[6] = rek[2] ^ rek[5];
+            rek[7] = rek[3] ^ rek[6];
+            if (++i == 10) {
+                Nr = 10;
+                return;
+            }
+            rek += 4;
+        }
+    }
+    rek[4] = GETWORD(cipherKey + 16);
+    rek[5] = GETWORD(cipherKey + 20);
+    if (keyBits == 192) {
+        for (;;) {
+            temp = rek[ 5];
+            rek[ 6] = rek[ 0] ^
+                (cTe4[(temp >> 16) & 0xff] & 0xff000000) ^
+                (cTe4[(temp >>  8) & 0xff] & 0x00ff0000) ^
+                (cTe4[(temp      ) & 0xff] & 0x0000ff00) ^
+                (cTe4[(temp >> 24)       ] & 0x000000ff) ^
+                crcon[i];
+            rek[ 7] = rek[ 1] ^ rek[ 6];
+            rek[ 8] = rek[ 2] ^ rek[ 7];
+            rek[ 9] = rek[ 3] ^ rek[ 8];
+            if (++i == 8) {
+                Nr = 12;
+                return;
+            }
+            rek[10] = rek[ 4] ^ rek[ 9];
+            rek[11] = rek[ 5] ^ rek[10];
+            rek += 6;
+        }
+    }
+    rek[6] = GETWORD(cipherKey + 24);
+    rek[7] = GETWORD(cipherKey + 28);
+    if (keyBits == 256) {
+        for (;;) {
+            temp = rek[ 7];
+            rek[ 8] = rek[ 0] ^
+                (cTe4[(temp >> 16) & 0xff] & 0xff000000) ^
+                (cTe4[(temp >>  8) & 0xff] & 0x00ff0000) ^
+                (cTe4[(temp      ) & 0xff] & 0x0000ff00) ^
+                (cTe4[(temp >> 24)       ] & 0x000000ff) ^
+                crcon[i];
+            rek[ 9] = rek[ 1] ^ rek[ 8];
+            rek[10] = rek[ 2] ^ rek[ 9];
+            rek[11] = rek[ 3] ^ rek[10];
+            if (++i == 7) {
+                Nr = 14;
+                return;
+            }
+            temp = rek[11];
+            rek[12] = rek[ 4] ^
+                (cTe4[(temp >> 24)       ] & 0xff000000) ^
+                (cTe4[(temp >> 16) & 0xff] & 0x00ff0000) ^
+                (cTe4[(temp >>  8) & 0xff] & 0x0000ff00) ^
+                (cTe4[(temp      ) & 0xff] & 0x000000ff);
+            rek[13] = rek[ 5] ^ rek[12];
+            rek[14] = rek[ 6] ^ rek[13];
+            rek[15] = rek[ 7] ^ rek[14];
+            rek += 8;
+        }
+    }
+    Nr = 0; // this should never happen
+}
+
 
 __device__ void load_roundkey(uint *s, uint *rk) {
     int tid = threadIdx.x;
