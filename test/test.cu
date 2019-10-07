468f1cfc7368ff9dfd1b5d72154537fba22fc1af
Bob Costas <finkzane@gmail.com>
2019-10-07 14:10:20 -0700

    added cpu equality operators

--- test/test.cu
+++ test/test.cu
@@ -3,18 +3,44 @@
 
 #include <string>
 #include <iostream>
+#include <cuda.h>
 
 #include "uint256_t.h"
 #include "AES.h"
 
-TEST_CASE( "test", "[catc]" )
+TEST_CASE( "uint256_t_eq_host", "[uint256_t]" )
 {
-    // AES *aes = new AES();
-
     uint256_t an_int;
     uint256_t an_int2;
 
-    an_int & an_int2;
+    bool eq = an_int == an_int2; 
+    bool neq = an_int != an_int2;
 
-    // delete aes;
+    REQUIRE( eq );
+    REQUIRE( !neq );
+
+    uint256_t a1;
+    uint256_t a2;
+
+    for( std::uint8_t idx = 0; idx < UINT256_SIZE_IN_BYTES; ++idx )
+        {
+            a1[ idx ] = idx;
+            a2[ idx ] = idx;
+        }
+
+    eq = a1 == a2;
+
+    REQUIRE( eq );
+
+    a1[ 0 ] = 0x01;
+    a2[ 0 ] = 0x02;
+
+    eq = a1 == a2;
+
+    REQUIRE( !eq );
+}
+
+TEST_CASE( "uint256_t_eq_dev", "[uint256_t]" )
+{
+    REQUIRE( true );
 }
