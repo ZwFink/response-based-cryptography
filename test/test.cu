fed5420e372f5f1b2b40e29a8ba69a3c70f9316f
Bob Costas <finkzane@gmail.com>
2019-10-07 21:27:04 -0700

    added == test for device

--- test/test.cu
+++ test/test.cu
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <cuda.h>
 
+#include "test_utils.h"
+
 #include "uint256_t.h"
 #include "AES.h"
 
@@ -42,5 +44,74 @@ TEST_CASE( "uint256_t_eq_host", "[uint256_t]" )
 
 TEST_CASE( "uint256_t_eq_dev", "[uint256_t]" )
 {
-    REQUIRE( true );
+    uint256_t a1;
+    uint256_t a2;
+
+    bool *result_code_dev = nullptr;
+    bool result_code = false;
+
+    uint256_t *a1_dev = nullptr;
+    uint256_t *a2_dev = nullptr;
+
+    cudaMalloc( (void**) &a1_dev, sizeof( uint256_t ) );
+    cudaMalloc( (void**) &a2_dev, sizeof( uint256_t ) );
+    cudaMalloc( (void**) &result_code_dev, sizeof( bool ) );
+
+    if( test_utils::HtoD( a1_dev, &a1, sizeof( uint256_t ) ) != cudaSuccess )
+        {
+            std::cout << "Failure to transfer a1 to device\n";
+        }
+
+    if( test_utils::HtoD( a2_dev, &a2, sizeof( uint256_t ) ) != cudaSuccess)
+        {
+            std::cout << "Failure to transfer a2 to device\n";
+        }
+    if( test_utils::HtoD( result_code_dev, &result_code, sizeof( bool ) ) != cudaSuccess)
+        {
+            std::cout << "Failure to transfer result_code to device\n";
+        }
+
+    test_utils::eq_kernel<<<1,1>>>( a1_dev,
+                                    a2_dev,
+                                    result_code_dev
+                                  );
+
+    if( test_utils::DtoH( &result_code, result_code_dev, sizeof( bool ) ) != cudaSuccess)
+        {
+            std::cout << "Failure to transfer to host \n";
+        }
+
+    REQUIRE( result_code );
+
+    result_code = false;
+
+    a1[ 0 ] = 0x02;
+
+    if( test_utils::HtoD( a1_dev, &a1, sizeof( uint256_t ) ) != cudaSuccess )
+        {
+            std::cout << "Failure to transfer a1 to device\n";
+        }
+
+    if( test_utils::HtoD( result_code_dev, &result_code, sizeof( bool ) ) != cudaSuccess)
+        {
+            std::cout << "Failure to transfer result_code to device\n";
+        }
+
+    test_utils::eq_kernel<<<1,1>>>( a1_dev,
+                                    a2_dev,
+                                    result_code_dev
+                                  );
+
+    if( test_utils::DtoH( &result_code, result_code_dev, sizeof( bool ) ) != cudaSuccess)
+        {
+            std::cout << "Failure to transfer to host \n";
+        }
+
+    REQUIRE( !result_code );
+
+
+    cudaFree( a1_dev );
+    cudaFree( a2_dev );
+    cudaFree( result_code_dev );
+
 }
