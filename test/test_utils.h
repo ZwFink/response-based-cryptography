f74af4316d394ef03161abb754f54078113c51a5
Bob Costas <finkzane@gmail.com>
2019-10-10 22:28:25 -0700

    documented binary/unary operations

--- test/test_utils.h
+++ test/test_utils.h
@@ -27,41 +27,78 @@ namespace test_utils
         }
 
         
+        /**
+         * Device function to perform a binary operation.
+         * @param Type the type whose binary operator we wish to test.
+         * @param func Binary operator member of Type that takes a member of Type.
+         * @param one The member of Type whose func will be called
+         * @param two The member of Type that will be passed into the 
+         *        func call 
+         * @returns the boolean result of the call, true or false
+         **/
         template<typename Type, bool (Type::*func)( Type )>
             __device__ bool bin_op_dev( Type *one, Type *two )
         {
             return (one->*func)( *two );
         }
 
+
+        /**
+         * Device function to perform a unary operation.
+         * @param Type the type on which to perform the operation.
+         * @param func Pointer to the  unary operator, 
+         *        which is a function member of Type to test
+         * @param one Pointer to the object to test.
+         * @returns a member of type Type, the result of the unary 
+         *          operator
+         **/
         template<typename Type, Type (Type::*func)()>
             __device__ Type unary_op_dev( Type *one )
         {
             return (one->*func)();
         }
 
+        /**
+         * A kernel to test a binary operator for class Type.
+         * @param Type The type whose binary operator to test
+         * @param func A pointer to Type's member function that 
+         *        takes a Type and returns bool
+         * @param one Pointer to the first variable to test
+         * @param two Pointer to the first variable to test
+         * @param dest Pointer to bool where the result should be stored
+         * @note (one->*func)( *two ) will be called by this function
+         **/
         template<class Type, bool (Type::*func)( Type )>
-    __global__ void binary_op_kernel( Type *one, Type *two,
-                                      bool *dest
-                                    )
-    {
-        bool comp = false;
+            __global__ void binary_op_kernel( Type *one, Type *two,
+                                              bool *dest
+                                            )
+            {
+                bool comp = false;
 
-        comp = bin_op_dev<Type, func>( one, two );
-        *dest = comp;
-    }
+                comp = bin_op_dev<Type, func>( one, two );
+                *dest = comp;
+            }
 
+
+        /**
+         * A kernel to test a unary operator for class Type.
+         * @param Type The type whose binary operator to test
+         * @param func Pointer to member function of Type to test.
+         *        this function takes no parameters, and returns the 
+         *        'unary-operated' one.
+         * @param one the member of Type on which to perform func
+         * @param dest The destination, where the result of func will 
+         *        be stored
+         **/
         template<class Type, Type (Type::*func)()>
             __global__ void unary_op_kernel( Type *one,
                                              Type *dest
-                                           )
-    {
-        Type ret = unary_op_dev<Type, func>( one );
+                                             )
+            {
+                Type ret = unary_op_dev<Type, func>( one );
 
-        *dest = ret;
-    }
-
-
-
+                *dest = ret;
+            }
 };
 
 #endif
