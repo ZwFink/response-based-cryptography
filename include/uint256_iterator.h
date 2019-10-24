b4ee0e19c2ba16fdec0f9ba50f7da5e18bff5aca
Bob Costas <finkzane@gmail.com>
2019-10-23 19:52:06 -0700

    added class members

--- include/uint256_iterator.h
+++ include/uint256_iterator.h
@@ -5,8 +5,13 @@
 class uint256_iter
 {
  public:
-    
-
+    uint256_t curr_perm;
+    uint256_t last_perm;
+    uint256_t t;
+    uint256_t tmp;
+    uint256_t key;
+    uint256_t corrupted_key;
+    bool overflow;
 };
 
 #endif // UINT256_ITERATOR_HH_INCLUDED
