12d32e3efb5f704232a094610bb33c53b70bb671
Jordan Wright <jaw566@nau.edu>
2019-11-02 16:24:16 -0700

    add get_bin_coef to perm_util

--- src/perm_util.cu
+++ src/perm_util.cu
@@ -11,21 +11,22 @@
 
 #include "uint256_t.h"
 
+// macro for min of two ints; used in binomial function
+#define min(a, b) ((a) > (b))? (b): (a)
+
 __device__ void decode_ordinal( uint256_t perm, 
                                 const uint256_t ordinal, 
                                 int mismatches, 
                                 int subkey_length )
 {
    uint256_t binom, curr_ordinal;   
-   // TODO: create copy constructor for uint256_t class
-   // Does not work as is: 
-   // curr_ordinal.set_all(ordinal);   
-   // perm.set_all(0);
+   curr_ordinal.copy( ordinal );
 
-   for( unsigned long bit = subkey_length-1; mismatches > 0; bit-- )
+   for( int bit = subkey_length-1; mismatches > 0; bit-- )
    {
       // compute the binomial coefficient n over k 
       // TODO: create binom_coef method for uint256_t class
+      
    }
 }
                             
@@ -64,4 +65,30 @@ __device__ void assign_last_permutation( uint256_t *perm,
    perm = perm << (subkey_length - mismatches);
 }
 
+// compute the binomial coefficient:
+// get the number of k-element subsets of an n-element set
+__device__ uint256_t get_bin_coef( int n, int k )  
+{
+   cudaError_t result = cudaSuccess;    
+   int *C = NULL;
+   int ret = -1;
+
+   result = cudaMalloc( (void**) &C, sizeof( int ) * k+1 );
+   assert( result == cudaSuccess );
+
+   C[0] = 1;
+
+   for( int i=1; i <= n; i++ )
+   {
+      for( int j=min(i,k); j>0; j-- )
+      {  
+         C[j] = C[j] + C[j-1];
+      }
+   }
+   ret = C[k];
+
+   cudaFree(C);
+
+   return ret; 
+}
                                 
