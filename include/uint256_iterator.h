bb6deb11bd991014601b98605e2107ecf0213448
Bob Costas <finkzane@gmail.com>
2019-11-12 16:09:38 -0700

    added constructor

--- include/uint256_iterator.h
+++ include/uint256_iterator.h
@@ -2,14 +2,30 @@
 #define UINT256_ITERATOR_HH_INCLUDED
 #include "uint256_t.h"
 
+#define CUDA_ONLY __device__
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER 
+#endif
+
+#define INLINE __forceinline__
+
+
 class uint256_iter
 {
  public:
+
+    CUDA_CALLABLE_MEMBER uint256_iter( const unsigned char *key,
+                                       const uint256_t& first_perm,
+                                       const uint256_t& final_perm
+                                     );
+
     uint256_t curr_perm;
     uint256_t last_perm;
     uint256_t t;
     uint256_t tmp;
-    uint256_t key;
+    uint256_t key_uint;
     uint256_t corrupted_key;
     bool overflow;
 };
--- src/uint256_iterator.cu
+++ src/uint256_iterator.cu
@@ -1,1 +1,12 @@
 #include "uint256_iterator.h"
+
+CUDA_CALLABLE_MEMBER uint256_iter::uint256_iter( const unsigned char *key,
+                                                 const uint256_t& first_perm,
+                                                 const uint256_t& final_perm
+                                               )
+{
+    curr_perm = first_perm;
+    last_perm = final_perm;
+    key_uint.from_string( key );
+    corrupted_key = key_uint ^ curr_perm;
+}
