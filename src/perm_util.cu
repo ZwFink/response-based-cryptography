38ce85ab9a52ba4dff28f114f1e7817f2c6f65b4
Jordan Wright <jaw566@nau.edu>
2019-11-11 17:56:43 -0700

    changes to perm_util

--- src/perm_util.cu
+++ src/perm_util.cu
@@ -1,4 +1,4 @@
-// utility file for permutation delimination
+// utility file for keyspace partitioning
 
 #ifndef PERM_UTIL_CU_
 #define PERM_UTIL_CU_
@@ -74,38 +74,7 @@ __device__ void get_perm_pair( uint256_t *starting_perm,
 
    if( pair_index == 0 )
    {
-      assig// C++ program for binomial coefficients 
-#include <stdio.h> 
-  
-// Function to print binomial table 
-int printbinomial(int max) 
-{ 
-    for (int m = 0; m <= max; m++) { 
-        printf("%2d", m); 
-        int binom = 1; 
-        for (int x = 0; x <= m; x++) { 
-  
-            // B(m, x) is 1 if either m or x is 
-            // is 0. 
-            if (m != 0 && x != 0) 
-  
-                // Otherwise using recursive formula 
-                // B(m, x) = B(m, x - 1) * (m - x + 1) / x 
-                binom = binom * (m - x + 1) / x; 
-  
-            printf("%4d", binom); 
-        } 
-        printf("\n"); 
-    } 
-} 
-  
-// Driver Function 
-int main() 
-{ 
-    int max = 10; 
-    printbinomial(max); 
-    return 0; 
-} n_first_permutation( starting_perm, mismatches );
+      assign_first_permutation( starting_perm, mismatches );
    } 
    else
    {
