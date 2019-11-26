55080179efe146e74e1dda51a8458042f55195af
Bob Costas <finkzane@gmail.com>
2019-11-26 01:53:57 -0800

    changed HtoD/DtoH so src and dest can be different types

--- include/cuda_utils.h
+++ include/cuda_utils.h
@@ -1,36 +1,22 @@
 #ifndef CUDA_UTILS_HH_INCLUDED
 #define CUDA_UTILS_HH_INCLUDED
+#include <cstddef>
 
 namespace cuda_utils
 {
-    template<class T>
-        cudaError_t HtoD( T *dest, T *src, std::size_t bytes )
+    template<typename T, typename V>
+        cudaError_t HtoD( T *dest, const V *src, std::size_t bytes )
     {
         cudaError_t err = cudaMemcpy( dest, src, bytes, cudaMemcpyHostToDevice );
         return err;
     }
 
-    template<class T>
-        cudaError_t DtoH( T *dest, T *src, std::size_t bytes )
+    template<typename T, typename V>
+        cudaError_t DtoH( T *dest, const V *src, std::size_t bytes )
     {
         cudaError_t err = cudaMemcpy( dest, src, bytes, cudaMemcpyDeviceToHost );
         return err;
     }
-
-    template<class T>
-        cudaError_t HtoD( T *dest, T *src )
-    {
-        cudaError_t err = cudaMemcpy( dest, src, sizeof( T ), cudaMemcpyHostToDevice );
-        return err;
-    }
-
-    template<class T>
-        cudaError_t DtoH( T *dest, T *src )
-    {
-        cudaError_t err = cudaMemcpy( dest, src, sizeof( T ), cudaMemcpyDeviceToHost );
-        return err;
-    }
-
 
 }; // namespace cuda_utils
 
