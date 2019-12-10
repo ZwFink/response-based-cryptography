37f990c0391032d6c4debca3cbd0a1a7a0894731
Bob Costas <finkzane@gmail.com>
2019-12-09 21:22:17 -0700

    corrected the number of parameters in header files

--- include/perm_util.h
+++ include/perm_util.h
@@ -15,15 +15,18 @@ __device__ void assign_first_permutation( uint256_t *perm, int mismatches );
 __device__ void assign_last_permutation( uint256_t *perm,
                                          int mismatches,
                                          int key_sz_bits );
+
 __device__ void get_perm_pair( uint256_t *starting_perm, 
                                uint256_t *ending_perm,
-                               std::size_t pair_index,        // thread id
+                               std::size_t pair_index,        // thread num
                                std::size_t pair_count,        // num threads
                                const int mismatches,           
                                const std::size_t keys_per_thread,
-                               const std::size_t key_sz_bits,        
+                               std::size_t key_sz_bits,        
+                               const std::uint64_t extra_keys,
                                const std::uint64_t total_perms
-                             );
+                              );
+
 
 
 #endif // PERM_UTIL_HH_INCLUDED
