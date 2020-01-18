5d4d0681045c1388de688815d85235cc455a4813
Bob Costas <finkzane@gmail.com>
2020-01-18 10:45:54 -0700

    inclusion of sbox, multiplication tables

--- include/sbox.h
+++ include/sbox.h
@@ -2,6 +2,10 @@
 #define __SBOX_TAB_HH
 using uchar = unsigned char;
 
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
 extern uchar Rcon[255];
 
 #ifdef USE_CONSTANT
@@ -22,7 +26,71 @@ __constant__
 __device__
 extern uchar Tsbox_64[256];
 
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uchar mul2[256];
 
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uchar mul_3[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uchar mul_9[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uchar mul_11[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uchar mul_13[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uchar mul_14[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uint cTe0[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uint cTe1[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uint cTe2[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uint cTe3[256];
+
+#ifdef USE_CONSTANT
+__constant__
+#endif
+__device__
+extern uint cTe4[256];
 
 
 #endif
